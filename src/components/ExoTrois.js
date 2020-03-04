d7c051ffee8edbc32a8aaff18776d23396aed68b
Ilya-tcv <chernychovilya@yahoo.fr>
2020-03-04 10:25:02 +0100

    Fin exo3

--- src/components/ExoTrois.js
+++ src/components/ExoTrois.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const ExoTrois = () => {
-     return (
-          <h1>Exo3</h1>
-     )
-}
+     var [user, setUser] = useState('inconnu');
+     var [value, setValue] = useState('')
 
-export default ExoTrois
+     const copie = (e) => {
+          user = e.target.value
+          setValue(user)
+     }
+
+	const changeUser = () => {
+          user = value
+          setUser(user)
+          setValue('')
+     };
+
+	return (
+		<div className='jumbotron'>
+			<h3 className='text-primary mb-3'>Welcome {user}</h3>
+			<input type='text' value={value} onChange={copie} />
+			<button className='btn btn-warning' onClick={()=>changeUser()}>
+				go
+			</button>
+		</div>
+	);
+};
+
+export default ExoTrois;
