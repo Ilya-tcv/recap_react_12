2001d09efa93369bd61def71929bacca177fecd7
Ilya-tcv <chernychovilya@yahoo.fr>
2020-03-04 11:43:10 +0100

    Fin exo4

--- src/components/ExoQuatre.js
+++ src/components/ExoQuatre.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ExoQuatre = () => {
-     return (
-          <h1>Exo4</h1>
-     )
-}
+     const [display, setDisplay] = useState({
+          class : "d-flex bg-danger w-25 p-4",
+          class2 : "d-none bg-danger w-25 p-4",
+          value :0
+     })
 
-export default ExoQuatre
+     const changerPlace = () => {
+          setDisplay({
+               class : "d-none bg-danger w-25 p-4",
+               class2 : "d-flex bg-danger w-25 p-4",
+               value : display.value +1 
+          })
+     }
+
+     const changerPlace2 = () => {
+          setDisplay({
+               class : "d-flex bg-danger w-25 p-4",
+               class2 : "d-none bg-danger w-25 p-4",
+               value : display.value +1 
+          })
+     }
+
+
+	return (
+		<div className='jumbotron'>
+			<h3>Déplacer les éléments</h3>
+			<div className='d-flex justify-content-between'>
+				<div className='w-50 p-5 mx-5 bg-primary'>
+					<div className={display.class}></div>
+				</div>
+				<div className='w-50 p-5 mx-5 bg-primary'>
+					<div className={display.class2}></div>
+				</div>
+			</div>
+               <div className='d-flex flex-column'>
+                    <button onClick={display.class === "d-none bg-danger w-25 p-4" ? changerPlace2 : changerPlace} className="btn btn-warning w-25 mx-auto m-2 text-center">Changer place</button>
+                    <button className="btn btn-success w-25 mx-auto m-2 text-center">Nbr de clicks :  {display.value} </button>
+               </div>
+		</div>
+	);
+};
+
+export default ExoQuatre;
